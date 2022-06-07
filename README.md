f500ba667813767cf5770af599319f637499a270
Rafael Ceron Espinosa <rafaelceronespinosa@Rafaels-MacBook-Pro.local>
2022-06-07 17:09:11 -0500

    [FEATURE] check s3 with X-RAY

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # SERVERLESS LAMBDA EXAMPLE 
 Some aws features using serverless framework 
-
-https://serverless-stack.com/chapters/tracing-serverless-apps-with-x-ray.html
+ 
+ - IAM
+ - Cloudwatch 
+ - X-Ray
--- users/routes.js
+++ users/routes.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let AWSXRay = require('aws-xray-sdk-core');
+let AWS = AWSXRay.captureAWS(require('aws-sdk'));
 
 // middleware that is specific to this router
 // define the home page route
-router.get('/all', function(req, res) {
-  console.log('this is an implementation');
-  res.status(404).json({ msg: 'not found' });
+router.get('/all', async (req, res) => {
+  const s3 = new AWS.S3();
+  s3.listBuckets((err, data) => {
+    if (err) {
+      console.log("Error", err);
+      res.status(500).json({ msg: 'could not fetch the values' });
+    } else {
+      console.log("Success", data.Buckets);
+      res.status(200).json({ msg: 'fetch all records' });
+    }
+  });
 });
 // define the about route
 router.post('/create', function(req, res) {
